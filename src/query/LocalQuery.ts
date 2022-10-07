4fc300eadf676d6758c18f1a1571efa8c3c5686e
maartenvandenbrande <maarten.vdbrande@gmail.com>
2022-10-07 14:44:00 +0200

    local query engine build

--- src/query/LocalQuery.ts
+++ src/query/LocalQuery.ts
@@ -5,21 +5,38 @@ import {QueryContext} from "../utils/queryContext";
 import {AggregatedQuery} from "./AggregatedQuery";
 import {Bindings} from "@comunica/bindings-factory";
 import {SolidClient} from "../classes/SolidClient";
+import {QueryExplanation} from "./queryExplanation";
+import {QueryEngine} from "@comunica/query-sparql";
 
 export class LocalQuery extends Query {
-  private logger = new Logger();
+  private logger = new Logger(loggerSettings);
   private readonly solidClient: SolidClient;
-
+  private queryEngine: QueryEngine | undefined;
 
   constructor(solidClient: SolidClient, queryContext: QueryContext, queryBindings?: Array<Bindings>) {
     super(queryContext, queryBindings);
-    new Logger(loggerSettings).info("AggregatedQuery");
+    this.logger.info("AggregatedQuery");
     this.solidClient = solidClient;
-  }
 
-  switchQueryType(): AggregatedQuery {
-    //TODO implement
-    return new AggregatedQuery(this.solidClient, this.queryContext, this.queryBindings);
+    const queryExplanation = new QueryExplanation(
+      queryContext.query,
+      queryContext.sources,
+      queryContext.comunicaVersion,
+      queryContext.comunicaContext,
+      queryContext.reasoningRules,
+      true
+    );
+
+    const queryEngineFactory = require(queryExplanation.comunicaVersion.toString()).QueryEngineFactory;
+
+    this.logger.debug("comunica context path = " + queryExplanation.comunicaContext);
+    new queryEngineFactory().create({
+      configPath: queryExplanation.comunicaContext,
+    }).then(async (queryEngine: QueryEngine) => {
+      this.queryEngine = queryEngine;
+      this.logger.debug(`Comunica engine build`);
+      
+    });
   }
 
   getBindings(): Promise<Bindings[]> {
@@ -27,6 +44,11 @@ export class LocalQuery extends Query {
   }
 
   streamBindings(callBackFn: (bindings: Bindings, addition: boolean) => void): void {
+
   }
 
+  switchQueryType(): AggregatedQuery {
+    //TODO implement
+    return new AggregatedQuery(this.solidClient, this.queryContext, this.queryBindings);
+  }
 }
